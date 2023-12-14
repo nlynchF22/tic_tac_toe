a744d2280ff7ba96ad229e8cfd68e27fd7ef4af2
WDAGUtilityAccount <WDAGUtilityAccount@cfae44d8-13cf-4264-942d-79b7110d0336.mshome.net>
2023-12-14 11:02:17 -0600

    First commit

---
+++ src/main/java/com/whhs/tic_tac_toe/SystemInfo.java
@@ -0,0 +1,13 @@
+package com.whhs.tic_tac_toe;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.whhs.tic_tac_toe {
+    requires javafx.controls;
+    exports com.whhs.tic_tac_toe;
+}
